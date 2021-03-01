60df19c6adea6b251d9d91083e8ac471d10405c0
Bapper-Ik <78368811+Bapper-Ik@users.noreply.github.com>
2021-03-01 11:51:42 +0100

    got it
    for the test

---
+++ code.js
@@ -0,0 +1,23 @@
+var qrcode = new QRCode(document.getElementById("qrcode"), {
+	height: 150,
+	width: 150,
+});
+
+function generate() {
+	const value = document.getElementById("TA");
+	if (!value.value) {
+		alert("Ooops Can't Generate for an Empty Space!");
+
+		value.focus();
+		return;
+	}
+
+	qrcode.makeCode(value.value);
+}
+
+function printing(qrimage) {
+	const qrimg = document.getElementById(qrimage).innerHTML;
+	w = window.open();
+	w.document.write(qrimg);
+	w.print();
+}
